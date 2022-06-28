7425491a37a5e9d5f3c3e6736a5d9f54a5fbaabb
ArtemCH240322 <sun2383@mail.ru>
2022-06-28 16:58:27 +0300

    Первый комит. Шаг 1-3

--- src/main/java/pro/sky/hw_2_5/Application.java
+++ src/main/java/pro/sky/hw_2_5/Application.java
@@ -3,11 +3,15 @@ package pro.sky.hw_2_5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
 
 
